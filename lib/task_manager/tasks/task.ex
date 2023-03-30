f367e93c8ec951217591eb19980fc12e9c532291
Ainur Galimov <ainur.galimov@dualbootpartners.com>
2023-03-30 23:47:59 +0500

    [#9] Add validations to changeset

--- lib/task_manager/tasks/task.ex
+++ lib/task_manager/tasks/task.ex
@@ -20,7 +20,27 @@ defmodule TaskManager.Tasks.Task do
   @doc false
   def changeset(task, attrs) do
     task
-    |> cast(attrs, [:title, :description, :end_task_date, :state, :priority])
+    |> cast(attrs, [
+      :title,
+      :description,
+      :end_task_date,
+      :state,
+      :priority,
+      :creator_id,
+      :performer_id
+    ])
     |> validate_required([:title, :description, :end_task_date, :state, :priority])
+    |> foreign_key_constraint(:creator_id)
+    |> foreign_key_constraint(:performer_id)
+    |> validate_change(:end_task_date, &validate_current_or_future_date/2)
+  end
+
+  def validate_current_or_future_date(field_name, %Date{} = end_task_date) do
+    end_task_date
+    |> Date.compare(Date.utc_today())
+    |> case do
+      :lt -> [{field_name, "Date in the past"}]
+      _ -> []
+    end
   end
 end
