5f1044daefdbeb20d3f0cfdfb7badefe9d5458d9
Ainur Galimov <ainur.galimov@dualbootpartners.com>
2023-06-22 13:01:19 +0500

    [#17] Add path-to-lcov

--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule TaskManager.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.lcov": :test
       ]
     ]
   end
